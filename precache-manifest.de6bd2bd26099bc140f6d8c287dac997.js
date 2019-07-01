d3b853aafcbad0fb195dddf43a8a934ccc548bae
MarkTension <tensen.mark@gmail.com>
2019-07-01 15:55:24 +0200

    Updates

---
+++ precache-manifest.de6bd2bd26099bc140f6d8c287dac997.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dd625cc68d9f98ee58d85272720f30",
+    "url": "/MarkTension/portfolio_web/index.html"
+  },
+  {
+    "revision": "4366ae610c5421f3b256",
+    "url": "/MarkTension/portfolio_web/static/css/main.01c78356.chunk.css"
+  },
+  {
+    "revision": "6cf11f4824e4d7c94e5e",
+    "url": "/MarkTension/portfolio_web/static/js/2.1ccdc38f.chunk.js"
+  },
+  {
+    "revision": "4366ae610c5421f3b256",
+    "url": "/MarkTension/portfolio_web/static/js/main.cf1f859d.chunk.js"
+  },
+  {
+    "revision": "f4d0fa3f01eacec2d01d",
+    "url": "/MarkTension/portfolio_web/static/js/runtime~main.47d05f97.js"
+  },
+  {
+    "revision": "0fc2ce35853c63f4a0d3d3be1321542d",
+    "url": "/MarkTension/portfolio_web/static/media/MIT.0fc2ce35.png"
+  },
+  {
+    "revision": "0c37d7c58f64c98bbb0ec3e4d29a5927",
+    "url": "/MarkTension/portfolio_web/static/media/cremey.0c37d7c5.png"
+  },
+  {
+    "revision": "6533b353fcf87c40f67b40fa73974845",
+    "url": "/MarkTension/portfolio_web/static/media/logo.6533b353.png"
+  },
+  {
+    "revision": "afa24e76ea074d677c8db9c277c79402",
+    "url": "/MarkTension/portfolio_web/static/media/plantvision.afa24e76.png"
+  },
+  {
+    "revision": "f07b61b4383bfe71a9335c249703007c",
+    "url": "/MarkTension/portfolio_web/static/media/uva.f07b61b4.png"
+  },
+  {
+    "revision": "ba399827eefef8150b83a11efe65e17d",
+    "url": "/MarkTension/portfolio_web/static/media/wordpress_ico.ba399827.png"
+  }
+]);
